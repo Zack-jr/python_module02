fe3bb8082a228ae734fa1a1590d01e38a90dde5a
Zack-jr <girardzack04@gmail,com>
2026-02-10 17:19:19 +0100

    ex4

---
+++ ex4/ft_raise_errors.py
@@ -0,0 +1,40 @@
+def check_plant_health(plant_name, water_level, sunlight_hours):
+    if plant_name is None:
+        raise ValueError("Plant name cannot be empty!")
+    elif water_level < 1:
+        raise ValueError(f"Water level {water_level} is too low (min 1)")
+    elif water_level > 10:
+        raise ValueError(f"Water level {water_level} is too high (max 10)")
+    elif sunlight_hours < 2:
+        raise ValueError(f"Sunlight hours {sunlight_hours} is too low (min 2)")
+    elif sunlight_hours > 12:
+        raise ValueError(f"Sunlight hours {sunlight_hours} is too high (max 12)")
+    else:
+        return (f"Plant '{plant_name}' is healthy!\n")
+
+def test_plant_checks():
+    print("=== Garden Plant Health Checker ===\n")
+    print("Testing good values...")
+    try:
+        print(check_plant_health("tomato", 8, 6))
+    except ValueError as e:
+        print(f"Error: {e}\n")
+    print("Testing empty plant name...")
+    try:
+        print(check_plant_health(None, 8, 2))
+    except ValueError as e:
+        print(f"Error: {e}\n")
+    print("Testing bad water level...")
+    try:
+        print(check_plant_health("cactus", 3000, 4))
+    except ValueError as e:
+        print(f"Error: {e}\n")
+    print("Testing bad sunlight hours...")
+    try:
+        print(check_plant_health("rose", 3, 2000))
+    except ValueError as e:
+        print(f"Error {e}\n")
+    print("All error raising tests completed!")
+
+if __name__ == '__main__':
+    test_plant_checks()
